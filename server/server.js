b61ca728808e254d64ede19b5552daf2b964c27c
Engineerbabu777 <awaismumtaz0099@gmail.com>
2024-09-03 17:56:14 +0500

    add: backend server, ok

---
+++ server/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+
+
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+
+
+app.use(express.json({ limit: "10mb" })); // allows you to parse the body of the request
+
+
+
+app.listen(PORT, () => {
+	console.log("Server is running on http://localhost:" + PORT);
+	// connectDB();
+});
